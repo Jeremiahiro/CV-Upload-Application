3eb7df896ef8bc31560c4d4d2411ff36ffffb3b3
jeremiahiro <jeremaihiro@gmail.com>
2022-03-20 08:22:36 -0500

    making it better

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
         'is_profile_complete' => \App\Http\Middleware\IsProfileComplete::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
     ];
 }
