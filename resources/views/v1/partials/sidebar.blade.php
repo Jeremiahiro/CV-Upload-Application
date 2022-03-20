dc7ce2dda195df13cc4d1ac9a88e01d9eb395604
jeremiahiro <jeremaihiro@gmail.com>
2022-03-20 06:37:58 -0500

    wip

--- resources/views/v1/partials/sidebar.blade.php
+++ resources/views/v1/partials/sidebar.blade.php
@@ -66,24 +66,22 @@
                 </a>
             </li>
         </ul>
-        @if (!auth()->user()->cv()->count())
-            <div class="py-4 mx-auto">
-                <a href="{{ route('cv.create') }}" class="btn btn-warning sidebar-btn px-4 font-weight-bold d-flex"
-                    style="font-weight: bold;">
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-                            viewBox="0 0 16 16" class="bi bi-plus fs-5 text-start">
-                            <path
-                                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z">
-                            </path>
-                        </svg>
-                    </span>
-                    <span class="d-none d-lg-block">
-                        ADD CV
-                    </span>
-                </a>
-            </div>
-        @endif
+        <div class="py-4 mx-auto">
+            <a href="{{ route('cv.create') }}" class="btn btn-warning sidebar-btn px-4 font-weight-bold d-flex"
+                style="font-weight: bold;">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
+                        viewBox="0 0 16 16" class="bi bi-plus fs-5 text-start">
+                        <path
+                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z">
+                        </path>
+                    </svg>
+                </span>
+                <span class="d-none d-lg-block">
+                    ADD CV
+                </span>
+            </a>
+        </div>
 		<div class="position-absolute bottom-0 pb-5">
 			<a
 				class="nav-link active text-light"
